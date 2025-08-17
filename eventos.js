8edefb5b6f5bcf3e5b4411ca9c01cfac8d0f97c1
melaniefrade <melanie.frade@estudiantes.utec.edu.uy>
2025-08-17 01:39:59 -0300

    Modificación de los saludos

--- eventos.js
+++ eventos.js
@@ -1,5 +1,11 @@
 let divSaludo = document.getElementById("div_saludo");
+let btnSaludo = document.getElementById("btn_saludo");
 
 divSaludo.addEventListener("click", () => {
   alert("Hola! Soy el div");
+});
+btnSaludo.addEventListener("click", (event) => {
+  //detener el evento para que no "siga" hacia el div
+  event.stopPropagation();
+  alert("Hola");
 });
